004771a3229e7c1c05f00412651e93b8ee9c50db
Carlos Giraldo <cgiraldo@expedia.com>
2019-07-23 21:22:28 -0400

    ✨ Use external library

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,22 @@
 javascript:(function(){
 
-    alert('hello world');
+    function main() {
+        alert('hello world');
+        html2canvas(document.querySelector("img")).then(canvas => {
+            canvas.id = 'helloWorld';
+            var container = document.querySelector(".bkWMgd");
+            container.insertAdjacentElement("afterend", canvas);
+        });
+    }
+
+    function dynamicallyLoadScript(url, callback) {
+        var scriptNode = document.createElement("script");
+        scriptNode.src = url;
+        scriptNode.onload = main;
+        document.body.appendChild(scriptNode);
+        callback();
+    }
+
+    dynamicallyLoadScript('https://html2canvas.hertzen.com/dist/html2canvas.min.js');
+
 })();
